d2464f870e318f617de5897cfa198fe94a5a0c52
MemanAwad <meman1awad@gmail.com>
2024-07-09 12:44:14 +0300

    add new handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
     raises a 401 error by using abort
     """
     abort(401)  # unautherized error
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/stats
+    Return:
+    raises a 401 error by using abort
+    """
+    abort(403)  # forbiden error
