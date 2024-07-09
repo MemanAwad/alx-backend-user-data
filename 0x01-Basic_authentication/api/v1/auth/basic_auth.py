6c5d15f1b95e053968e29c2eddc8c4e5e41453a4
MemanAwad <meman1awad@gmail.com>
2024-07-10 01:54:52 +0300

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,6 +44,6 @@ class BasicAuth(Auth):
         if not isinstance(decoded_base64_authorization_header, str):
             return None, None
         if ':' not in decoded_base64_authorization_header:
-                return None, None
+            return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
