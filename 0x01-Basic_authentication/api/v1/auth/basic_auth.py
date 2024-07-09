c80c2ab13f5fd18aed76520cd9fabf292246c97b
MemanAwad <meman1awad@gmail.com>
2024-07-10 01:39:31 +0300

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of basic_auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
             return authorization_header[len(prefix):]
         else:
             return None
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        '''encode the text'''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, ValueError):
+            return None 
