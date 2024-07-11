57b486dd7a9312f5b7a775f5eac38e82a34a691e
MemanAwad <meman1awad@gmail.com>
2024-07-11 15:37:48 +0300

    add new method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """ Module of SessionAuth
 """
 from api.v1.auth.auth import Auth
-import base64
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''SessionAuth class'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a Session ID for a user_id'''
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return session_id
