09e556d8fc42d6373b3fa31132b4bc48966702ca
MemanAwad <meman1awad@gmail.com>
2024-07-16 21:39:23 +0300

    add new function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,13 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+        '''hash password that return bytes'''
+        password_bytes = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password_bytes, salt)
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -13,18 +20,12 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        '''hash password that return bytes'''
-        password_bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password_bytes, salt)
-
     def register_user(self, email: str, password: str) -> User:
         '''method that register users'''
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            passw = self._hash_password(password)
+            passw = _hash_password(password)
             user = self._db.add_user(email, passw)
             return user
